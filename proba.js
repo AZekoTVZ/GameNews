c159c3e221bca9a69d20f4caed4523d1b920bd1f
MarinCader <mcader@tvz.hr>
2024-08-20 23:53:26 +0200

    Unos konstruktora
    sadrži atribut "ime"

--- proba.js
+++ proba.js
@@ -1,6 +1,9 @@
 class Proba{
+    constructor(ime) {
+        this.ime = ime;
+    }
     Pozdrav(){
-        console.log("Hello there");
+        console.log(`Hello there ${ime}`);
     }
     GithubDesktop(){
         console.log("GithubDesktop povezan!💪");
