6072ed1e17388b1c327dfeebe3f7a8cc0feb9fe5
MarinCader <mcader@tvz.hr>
2024-08-21 00:10:45 +0200

    izbrisan redak
    redak je bio prazan

--- proba.js
+++ proba.js
@@ -9,6 +9,5 @@ class Proba{
         console.log("GithubDesktop povezan!💪");
     }
 }
-const p1 = new Proba("Zeko");
-p1.Pozdrav();
+const p1 = new Proba("Zeko"); p1.Pozdrav();
 const p2 = new Proba("Marin");
